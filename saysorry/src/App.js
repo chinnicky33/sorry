128bf0ff30f744bc3a6ae78291e1148b3f03b1a4
vinayreddy <vinay.reddy@borderfree.io>
2020-12-04 18:00:10 +0530

    ff

---
+++ saysorry/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div id="marquee">
+    <div>
+        <span>I LOVE U JAANU</span>
+    </div>
+    <div aria-hidden="true">
+        <span style={{fontcolor:"red"}}>I M SORRY JAANU</span>
+    </div>
+  <div aria-hidden="true">
+        <span>I LOVE U JAANU</span>
+    </div>
+</div>
+    </div>
+  );
+}
+
+export default App;
